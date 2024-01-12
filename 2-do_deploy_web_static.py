6fa8f009d9a36dff6300d6a65fd1077a7bdb1533
dev-tch <tijeni.chaabeni@gmail.com>
2024-01-11 16:47:39 -0800

    deploy script clean version

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,19 +20,23 @@ def do_deploy(archive_path):
         file_with_ext = archive_path.split("/")[-1]
         file_no_ext = file_with_ext.split(".")[0]
         # get src and path
-        src_path = "/tmp/{}".format(file_with_ext)
-        dest_path = "/data/web_static/releases/{}".format(file_no_ext)
+        serv_file = "/tmp/{}".format(file_with_ext)
+        dir_unzip = "/data/web_static/releases/{}/".format(file_no_ext)
         # upload achive to my remote server
-        put(src_path)
+        put(archive_path, "/tmp/")
+        # create new directory
+        run("mkdir -p {}".format(dir_unzip))
         # uncompress the archive
-        run("tar -xzf {}  -C {}".format(src_path, dest_path))
-        # delete achive
-        run("rm {}".format(src_path))
+        run("tar -xzf {}  -C {}".format(serv_file, dir_unzip))
+        # move uncompressed files to dir_unzip
+        run("mv {}/web_static/* {}/".format(dir_unzip, dir_unzip))
+        # remove folder web_static in dir_unzip
+        run("rm -rf {}/web_static".format(dir_unzip))
         # delete symbolic link
         link = "/data/web_static/current"
         run("rm -rf {}".format(link))
         # recreate new symbolic link
-        run("ln -s {} {}".format(dest_path, link))
+        run("ln -s {}/ {}".format(dir_unzip, link))
         return True
     except Exception as e:
         return False
