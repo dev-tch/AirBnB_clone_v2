d0f7eadf29789c74c255e79722bfc286cee619b9
dev-tch <tijeni.chaabeni@gmail.com>
2024-01-26 21:38:44 +0000

    pass param in url flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""""
+first module flask
+"""
+from flask import Flask
+
+
+# create flast application
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def get_hello_hbnb():
+    """
+    return message 'hello HBNB!'
+    when web server invoking router '/'
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def get_hbnb():
+    """
+    return message HBNB
+    when web server invoking router '/hbnb'
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def get_c_text(text):
+    """
+    passing parameters  in flask rounting
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
