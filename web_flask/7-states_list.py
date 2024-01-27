1db42b38408d482742c1611d0d9c74bcfb62f802
dev-tch <tijeni.chaabeni@gmail.com>
2024-01-27 12:15:34 -0800

    pass list to view

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+module web application flask 
+"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+app.route('/states_list', strict_slashes=False)
+def get_states_list():
+    """
+    pass list states to view  
+    """
+    states = storage.all('State').values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('states_list.html', states=sorted_states)
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
