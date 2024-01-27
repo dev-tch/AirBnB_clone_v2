302675c1b0917c50461ac27b1d79b1ee6fb31d77
dev-tch <tijeni.chaabeni@gmail.com>
2024-01-27 00:58:53 +0000

    pass two variables to view

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+""""
+first module flask
+"""
+from flask import Flask, render_template
+
+
+# create flast application
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def get_hello_hbnb():
+    """
+    return message 'hello HBNB!'
+    when web server invoking router '/'
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def get_hbnb():
+    """
+    return message HBNB
+    when web server invoking router '/hbnb'
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def get_c_text(text):
+    """
+    passing parameters  in flask rounting
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python/', strict_slashes=False,
+           defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def get_python__text(text):
+    """
+    passing parameters  in flask rounting
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def get_number_n(n):
+    """
+    return int value
+    """
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def get_number_template_n(n):
+    """
+    pass variable to template htmp
+    """
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def get_number_odd_or_even_n(n):
+    """
+    pass variable to template htmp
+    """
+    odd_even = "even" if n % 2 == 0 else "odd"
+    return render_template('6-number_odd_or_even.html',
+                           number=n, parity=odd_even)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
